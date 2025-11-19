e327eb42fc511dab0e3de6adc0ff3d4f82a39cf3
Administrator <418463157@qq.com>
2025-11-19 09:52:49 +0800

    update(login):page

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,15 @@
 <template lang="">
     <div class="login w-full h-full ">
-        <div class="fixed top-1/2 bg-white shadow-md left-1/2 -translate-x-1/2 flex flex-col gap-8 -translate-y-1/2  px-10 py-10 rounded-md">
-            <div class="flex">
-                <div class="flex flex-col gap-2" @click="activeTab = 'Login'">登录</div>
-                <div class="flex flex-col gap-2" @click="activeTab = 'Register'">注册</div>
+        <div class="fixed top-1/2 w-[400px] h-[480px] bg-white left-1/2 -translate-x-1/2 flex flex-col gap-8 -translate-y-1/2  px-10 py-10 rounded-md neumorphism-up ">
+            <div class="login-toggle">
+                <input type="checkbox" v-model="activeTab" id="bluetooth">
+                <label for="bluetooth"></label>
             </div>
 
-            <input type="text" class="border border-black px-2 rounded-md" placeholder="用户名" v-model="loginForm.username">
-            <input type="password" class="border border-black px-2 rounded-md" placeholder="密码" v-model="loginForm.password">
-            <input type="text" class="border border-black px-2 rounded-md" placeholder="邮箱" v-if="activeTab === 'Register'" v-model="loginForm.email">
-            <button type="submit" class="border border-black px-2 rounded-md" @click="handleLogin">提交</button>
+            <input type="text" class="input" placeholder="用户名" v-model="loginForm.username">
+            <input type="password" class="input" placeholder="密码" v-model="loginForm.password">
+            <input type="text" class="input" placeholder="邮箱" v-if="activeTab" v-model="loginForm.email">
+            <button type="submit" @click="handleLogin">提交</button>
         </div>
     </div>
 </template>
@@ -18,8 +18,9 @@ import { reactive, ref } from 'vue'
 import { useUserStore } from '@/store/modules/user'
 import router from '@/router'
 import serverApi from '@/api'
+// https://uiverse.io/elements?tags=neumorphism&page=2
 
-const activeTab = ref('Login')
+const activeTab = ref(false)
 
 const userStore = useUserStore()
 const loginForm = reactive({
@@ -28,12 +29,13 @@ const loginForm = reactive({
     email: ''
 })
 
+
 const handleLogin = () => {
     console.log(serverApi)
-    serverApi[activeTab.value](loginForm).then((res: any) => {
+    serverApi[activeTab.value ? 'Register' : 'Login'](loginForm).then((res: any) => {
         console.log(res.data)
-        activeTab.value === 'Login' && userStore.login(res.data)
-        activeTab.value === 'Login' && router.push('/')
+        // !activeTab.value && userStore.login(res.data)
+        !activeTab.value && router.push('/')
     })
 }
 
