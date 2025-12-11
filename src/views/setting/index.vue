275c57b49f24e937cad2db97322149e0b3722824
Administrator <418463157@qq.com>
2025-12-11 13:46:18 +0800

    update

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="w-full text-sm">
-        <div v-for="(item, k) in userStore.userInfo" :key="k" class="flex gap-6">
-            <div class="w-[120px] text-right">{{ k }}:</div>
-            <div class="flex-1">{{ item }}</div>
+    <div class="w-full text-sm flex items-center justify-center h-full">
+        <div class="w-1/2">
+            <div v-for="(item, k) in userStore.userInfo" :key="k" class="flex gap-4">
+                <div class="w-[120px] text-right">{{ k }}:</div>
+                <div class="flex-1 w-full auto-wrap">{{ item }}</div>
+            </div>
         </div>
     </div>
 </template>
@@ -11,6 +13,10 @@ import { useUserStore } from '@/store/modules/user'
 const userStore = useUserStore()
 
 </script>
-<style lang="">
-
+<style lang="scss">
+.auto-wrap {
+  word-wrap: break-word;      /* 长单词换行 */
+  word-break: break-word;         /* 单词边界换行 */
+  white-space: break-spaces;        /* 空白符正常处理 */
+}
 </style>
