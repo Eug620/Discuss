241e0c84d020613519e1df35ca2d9535ee404edb
yeyuhang <yeyuhang@zzltsw.com>
2026-06-15 14:12:24 +0800

    feat(login): style

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-full text-sm flex items-center justify-center h-full relative">
-        <div class="w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border border-gray-300">
+    <div class="w-full text-sm h-full relative p-2">
+        <div class="w-full border border-gray-300">
             <div v-for="(item, k) in userStore.userInfo" :key="k" class="flex border-b border-gray-300 last:border-b-0">
                 <div class="w-[120px] text-right border-r border-gray-300 p-3 pr-4">{{ k }}:</div>
                 <div class="flex-1 w-full auto-wrap p-3 pl-4">{{ item }}</div>
