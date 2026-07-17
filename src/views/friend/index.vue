673374e6dc746003e16709600d291c6f884f686e
yeyuhang <yeyuhang@zzltsw.com>
2026-07-17 13:44:32 +0800

    fix(friend): info update

--- src/views/friend/index.vue
+++ src/views/friend/index.vue
@@ -93,7 +93,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
           <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
         </svg>
-        {{ dayjs(getFriendInfo.createdAt).fromNow() }}
+        {{ dayjs(getFriendInfo.createdAt).format('YYYY/MM/DD') }}
       </div>
       <div class="text-sm flex gap-1 items-center" >
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
