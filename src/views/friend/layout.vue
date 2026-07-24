1711706aef937f0131ea3aa81a4b31de7cf8ed51
yeyuhang <yeyuhang@zzltsw.com>
2026-07-24 14:00:20 +0800

    feat(layout): update

--- src/views/friend/layout.vue
+++ src/views/friend/layout.vue
@@ -27,11 +27,18 @@
           v-for="friend in getFriends" 
           @click="handleFriendClick(friend)" 
           :key="friend.id"
-          class="cursor-pointer p-2 hover:bg-gray-200 truncate animate__flipInX animate__animated" 
+          class="cursor-pointer p-2 hover:bg-gray-200 truncate animate__flipInX animate__animated flex items-center" 
           :class="{
             'bg-gray-200': paramsID === friend.friend_info.id, 
             'text-green-700': friendStore.getFriendMap[friend.friend_info.id]?.status
-          }">{{ friend.friend_info.username }}</div>
+          }">
+          <span>{{ friend.friend_info.username }}</span>
+          <div class="w-0 overflow-hidden py-1 mx-auto">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+            </svg>
+          </div>  
+        </div>
         </div>
       </div>
       <div class="flex-auto animate__fadeIn animate__animated">
