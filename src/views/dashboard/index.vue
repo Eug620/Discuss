61aa095c6778cb58123676cdac882c04bf64f0d2
yeyuhang <yeyuhang@zzltsw.com>
2026-07-27 10:22:57 +0800

    style(dashboard): update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,11 @@
 <template lang="">
     <div class="dashboard p-2 h-full overflow-y-auto">
         <div 
-            class="text-sm group mb-1 flex items-end animate__fadeIn animate__animated" 
+            class="text-sm group flex items-end animate__fadeIn animate__animated" 
             v-for="(item, idx) in alertStore.alerts" 
             :key="idx"
         >
-            <span class="mr-2"><span class="bg-gray-200">[{{item.sender}}]</span>：{{item.message}}</span>
+            <span class="mr-2">[{{item.sender}}]：{{item.message}}</span>
             <span class="hidden text-xs group-hover:block text-gray-600 cursor-pointer" @click="alertStore.alerts.splice(idx, 1)">{{ dayjs(item.timestamp).format('YYYY/MM/DD HH:mm:ss') }}</span>
         </div>
     </div>
