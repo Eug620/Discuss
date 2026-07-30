e1d68a29e1cb394a0f8cab41aa4ae8bc52077b4a
yeyuhang <yeyuhang@zzltsw.com>
2026-07-30 14:42:34 +0800

    style(dashboard): update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,14 +1,15 @@
 <template lang="">
     <div class="dashboard p-2 h-full overflow-y-auto">
         <div 
-            class="text-sm group gap-2 flex items-end animate__fadeIn animate__animated" 
+            class="text-sm group gap-1 flex items-center animate__fadeIn animate__animated" 
             v-for="(item, idx) in alertStore.alerts" 
             :key="idx"
         >
-            <span>{{ dayjs(item.timestamp).format('YYYY/MM/DD HH:mm:ss') }} [{{item.sender}}]：{{item.message}}</span>
-            <svg @click="alertStore.alerts.splice(idx, 1)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 cursor-pointer hidden group-hover:block">
+            <span>[{{item.sender}}]：{{item.message}}</span>
+            <svg @click="alertStore.alerts.splice(idx, 1)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 bg-gray-100 cursor-pointer hidden group-hover:block">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
+            <div class="flex-1 text-right hidden group-hover:block">{{ dayjs(item.timestamp).format('YYYY/MM/DD HH:mm:ss') }}</div>
         </div>
     </div>
 </template>
