ae6afcffad3145aec8c21fb708b22a57bf2be8be
yeyuhang <yeyuhang@zzltsw.com>
2026-07-30 09:24:21 +0800

    feat(dashboard): update

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,12 +1,14 @@
 <template lang="">
     <div class="dashboard p-2 h-full overflow-y-auto">
         <div 
-            class="text-sm group flex items-end animate__fadeIn animate__animated" 
+            class="text-sm group gap-2 flex items-end animate__fadeIn animate__animated" 
             v-for="(item, idx) in alertStore.alerts" 
             :key="idx"
         >
-            <span class="mr-2">[{{item.sender}}]：{{item.message}}</span>
-            <span class="hidden text-xs group-hover:block text-gray-600 cursor-pointer" @click="alertStore.alerts.splice(idx, 1)">{{ dayjs(item.timestamp).format('YYYY/MM/DD HH:mm:ss') }}</span>
+            <span>{{ dayjs(item.timestamp).format('YYYY/MM/DD HH:mm:ss') }} [{{item.sender}}]：{{item.message}}</span>
+            <svg @click="alertStore.alerts.splice(idx, 1)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 cursor-pointer hidden group-hover:block">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
         </div>
     </div>
 </template>
--- src/views/room/index.vue
+++ src/views/room/index.vue
@@ -68,7 +68,7 @@
           <div class="flex-1">
               <div class="border-b border-gray-300 p-2 pl-0 animate__flipInX animate__animated">群成员</div>
               <div v-for="member in getMember" :key="member.user_id"
-                  class="text-sm flex gap-1 items-center py-1 hover:bg-gray-200 my-1 rounded-md" :class="{
+                  class="group text-sm flex gap-1 items-center py-1 hover:bg-gray-200 my-1 rounded-md" :class="{
                     'underline underline-offset-2': member.user_id === userStore.userInfo.id,
                     'text-green-700': getMemberOnline.has(member.user_id)
                   }">
@@ -85,7 +85,7 @@
                   <span class="flex-1 gap-1 items-center truncate">{{ member.user_info.username }}</span>
 
                   <template v-if="getRoomInfo.creator !== member.user_id && roomStore.roomsMine.find((item: any) => item.id === route.params.id)">
-                    <svg @click="handleRemoveMember(member.id)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 cursor-pointer">
+                    <svg @click="handleRemoveMember(member.id)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 cursor-pointer hidden group-hover:block">
                       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                     </svg>
                   </template>
